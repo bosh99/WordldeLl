5a41d538d7ed0d5a6e1bb800c1b31bc9fb2e6db2
Manuel Villegas <manuel.villegas.michel@gmail.com>
2022-09-23 13:32:51 -0500

    Final

--- Banco.hpp
+++ Banco.hpp
@@ -5,12 +5,35 @@ using namespace std;
 
 class Banco {
     public:
-        string bancoPalabras[10] = {"PARRA", "LUNA", "ORNITORRINCO", "A", "BALON", "DORITOS", "PUENTE", "COMPUTADOR", "AUTOMOVIL", "EDIFICIO"}; // Aqui se le pueden agregar palabras
+        string bancoRandom[10] = {"PARRA", "LUNA", "ORNITORRINCO", "A", "BALON", "DORITOS", "PUENTE", "COMPUTADOR", "AUTOMOVIL", "EDIFICIO"};
+        string bancoAnimales[10] = {"LEON", "ELEFANTE", "TIGRE", "LORO", "CACATUA", "GATO", "MONO", "PERRO", "PAVO", "CHIGUIRO"};
+        string bancoProgramacion[10] = {"PYTHON", "JAVA", "HARDWARE", "SOFTWARE", "SCALA", "MOO", "C", "RUBY", "HASKELL", "LINUX"};
 
-        // Seleccionar palabra aleatoriamente
         string palabraRandom(){
             srand((unsigned)time(NULL));
-            string palabra = bancoPalabras[(rand()%10)];
-            return palabra;
+            int input;
+            cout<<"INGRESA QUE MODO QUIERES JUGAR: "<< endl;
+            cout<<"[1] -> PALABRAS RANDOM"<< endl;
+            cout<<"[2] -> ANIMALES"<< endl;
+            cout<<"[3] -> PALABRAS RELACIONADAS CON PROGRAMACION"<< endl;
+            cin>>input;
+
+            if(input==1) {
+                string palabra = bancoRandom[(rand()%10)];
+                return palabra;
+            }
+            else if(input==2) {
+                string palabra = bancoAnimales[(rand()%10)];
+                return palabra;
+            }
+            else if(input==3) {
+                string palabra = bancoProgramacion[(rand()%10)];
+                return palabra;
+            }
+            else {
+                cout<<"ERROR EN EL INPUT, SE SELECCIONO UNA PALABRA RANDOM"<<endl;
+                string palabra = bancoRandom[(rand()%10)];
+                return palabra;
+            }
         }
 };
